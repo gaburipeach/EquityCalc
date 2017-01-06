0925fa7f4ef4e335f77368615cb64e0672881a2c
Charles Tan <Charles Tan>
2017-01-06 01:08:19 -0500

    Changed is methods to handle 7 cards

--- Showdown.py
+++ Showdown.py
@@ -23,7 +23,7 @@ class Showdown(object):
         # Board is a list of cards
         self.board = board
         # Rank is organized as [Best combo, Best Primary, Best Secondary, Kicker]
-        self.rank = [BoardScore.high_card, None, None, 8]
+        self.rank = [BoardScore.high_card, 0, 0, 0, 0, 0]
         self.winners = None
 
     @classmethod
@@ -38,13 +38,16 @@ class Showdown(object):
         """
         Finds if five-card combo contains a pair.
         Returns true if there is at least a pair in the combo.
-        
+
         """
         combo_values = self.retrieve_values(combo)
+        combo_values.sort() #WHOOO
         for card in combo_values:
-            if combo_values.count(card) >= 2:
-                return True
-        return False
+            if combo_values.count(card) == 2:
+                combo_values.remove(card)
+                combo_values.remove(card)
+                return True, [card, card, combo_values[4], combo_values[3], combo_values[2]]
+        return False, []
 
     # %%Checked%%
     def is_two_pair(self, combo):
@@ -52,16 +55,26 @@ class Showdown(object):
         Finds if five-card combo contains two pairs.
         """
         combo_values = self.retrieve_values(combo)
+        combo_values.sort() #WHOO
+        combo_values.reverse()
+        pair1 = None
+        pair2 = None
         for card in combo_values:
             if combo_values.count(card) >= 2:
+                # Removing while iterating is ok here because there are only 5
+                # cards and by removing we skip 1 iteration, so max there will
+                # only be 3 cards left and it doesn't matter if we skip 1
                 combo_values.remove(card)
                 combo_values.remove(card)
-                new_combo_values = combo_values[:]
-                for new_card in new_combo_values:
-                    if new_combo_values.count(new_card) >= 2:
-                        return True
-                return False
-        return False
+                if not pair1:
+                    pair1 = card
+                else:
+                    pair2 = card
+                if pair2:
+                    if pair1 < pair2:
+                        pair1, pair2 = pair2, pair1
+                    return True, [pair1, pair1, pair2, pair2, combo_values[0]]
+        return False, []
 
     # %%Checked%%
     def is_three_kind(self, combo):
@@ -69,10 +82,14 @@ class Showdown(object):
         Finds if five-card combo contains three of a kind.
         """
         combo_values = self.retrieve_values(combo)
+        combo_values.sort() #WHOO
         for card in combo_values:
             if combo_values.count(card) >= 3:
-                return True
-        return False
+                combo_values.remove(card)
+                combo_values.remove(card)
+                combo_values.remove(card)
+                return True, [card, card, card, combo_values[3], combo_values[2]]
+        return False, []
 
     @classmethod
     def is_sequential(cls, sorted_board):
@@ -85,24 +102,31 @@ class Showdown(object):
         return all(a == b for a, b in enumerate(it, first + 1))
 
     # %%Checked%%
-    def is_straight(self, combo):
+    def is_straight(self, combo, straight_flush=None):
         """
         Checks if 5-card combo contains a straight.
 
         """
         # Sorts the list of cards
-        sorted_values = self.retrieve_values(combo)
+        if not straight_flush:
+            sorted_values = self.retrieve_values(combo)
+        else:
+            sorted_values = combo
         sorted_values.sort()
         # Checks if list is sequential
-        is_seq_straight = self.is_sequential(sorted_values)
+        for i in range(len(combo)-5, -1, -1):
+            is_seq_straight = self.is_sequential(sorted_values[i:i+5])
+            if is_seq_straight:
+                ans = sorted_values[i:i+5]
+                ans.reverse()
+                return True, ans
         # Checks edge case of A2345
-        if sorted_values == [2, 3, 4, 5, 14]:
-            is_seq_straight = True
-        return is_seq_straight
+        if set([2, 3, 4, 5, 14]).issubset(set(sorted_values)):
+            return True, [5, 4, 3, 2, 14]
+        return False, []
 
-    # %%Checked%%
-    @classmethod
-    def is_flush(cls, combo):
+
+    def is_flush(self, combo, num=5):
         """
         Checks if 5-card combo contains a flush
 
@@ -110,10 +134,24 @@ class Showdown(object):
         Combo[0] contains the board, which contains a list of cards (card, card, card...).
 
         """
-        if (combo[0].suit == combo[1].suit and combo[0].suit == combo[2].suit and
-                    combo[0].suit == combo[3].suit and combo[0].suit == combo[4].suit):
-            return True
-        return False
+        suit_count = [0, 0, 0, 0]
+        for card in combo:
+            suit_count[card.suit-1]+=1
+        f_suit = None
+        for i in range(4):
+            if suit_count[i] >= 5:
+                f_suit = i+1
+        if f_suit == None:
+            return False, []
+        ans = []
+        for card in combo:
+            if card.suit == f_suit:
+                ans.append(card)
+        ans = self.retrieve_values(ans)
+        ans.sort()
+        ans.reverse()
+        return True, ans[:min([num, len(ans)])]
+
 
     # %%Checked%%
     def is_full_house(self, combo):
@@ -122,16 +160,22 @@ class Showdown(object):
 
         """
         combo_values = self.retrieve_values(combo)
+        combo_values.reverse()
+        new_combo_values = None
+        three_of_kind_card = None
         for card in combo_values:
             if combo_values.count(card) == 3:
                 combo_values.remove(card)
                 combo_values.remove(card)
                 combo_values.remove(card)
+                three_of_kind_card = card
                 new_combo_values = combo_values[:]
-                if new_combo_values[0] == new_combo_values[1]:
-                    return True
-                return False
-        return False
+                break
+        if new_combo_values:
+            for card in new_combo_values:
+                if new_combo_values.count(card) >= 2:
+                    return True, [three_of_kind_card, three_of_kind_card, three_of_kind_card, card, card]
+        return False, []
 
     # %%Checked%%
     def is_four_kind(self, combo):
@@ -140,10 +184,22 @@ class Showdown(object):
 
         """
         combo_values = self.retrieve_values(combo)
+        combo_values.sort()
         for card in combo_values:
             if combo_values.count(card) == 4:
-                return True
-        return False
+                combo_values.remove(card)
+                combo_values.remove(card)
+                combo_values.remove(card)
+                combo_values.remove(card)
+                return True, [card, card, card, card, combo_values[2]]
+        return False, []
+
+
+    def is_straight_flush(self, combo):
+        flush_list = self.is_flush(combo, 7)[1]
+        if len(flush_list) >= 5:
+            return self.is_straight(flush_list, 1)
+        return False, []
 
     # %%Checked%%
     def find_possible_combos(self, hand):
--- TestShowdown.py
+++ TestShowdown.py
@@ -32,143 +32,179 @@ class TestShowdown(TestCase):
 
     def test_is_pair1(self):
         # Test if method detects pair
-        gen = [Card(2, 1), Card(3, 1), Card(3, 1), Card(4, 1), Card(5, 1)]
+        gen = [Card(2, 1), Card(3, 1), Card(3, 1), Card(4, 1), Card(5, 1), Card(12, 1), Card(9, 4)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_pair(gen))
+        self.assertEqual((True, [3,3,12,9,5]), s.is_pair(gen))
 
     def test_is_pair2(self):
         # Test if method detects no pair
-        gen = [Card(2, 1), Card(3, 4), Card(6, 1), Card(4, 2), Card(5, 1)]
+        gen = [Card(2, 1), Card(3, 4), Card(6, 1), Card(4, 2), Card(5, 1), Card(8, 2), Card(10,1)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_pair(gen))
+        self.assertEqual((False, []), s.is_pair(gen))
 
     def test_is_pair3(self):
-        # Test if method detects pair from three of a kind
-        gen = [Card(2, 1), Card(3, 3), Card(4, 3), Card(4, 4), Card(4, 2)]
+        # Test if method detects pair
+        gen = [Card(2, 1), Card(5, 3), Card(4, 3), Card(7, 4), Card(4, 2), Card(10,2), Card(11,3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_pair(gen))
+        self.assertEqual((True, [4,4,11,10,7]), s.is_pair(gen))
 
     def test_is_two_pair1(self):
         # Test if method detects 2 pair
-        gen = [Card(14, 3), Card(3, 2), Card(3, 4), Card(4, 2), Card(14, 3)]
+        gen = [Card(14, 3), Card(3, 2), Card(3, 4), Card(4, 2), Card(14, 3), Card(6, 1), Card(7,2)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_two_pair(gen))
+        self.assertEqual((True, [14, 14, 3, 3, 7]), s.is_two_pair(gen))
 
     def test_is_two_pair2(self):
         # Test if method detects no 2 pair
-        gen = [Card(14, 3), Card(3, 2), Card(3, 4), Card(4, 2), Card(13, 3)]
+        gen = [Card(14, 3), Card(3, 2), Card(3, 4), Card(4, 2), Card(13, 3), Card(2, 4), Card(6, 2)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_two_pair(gen))
+        self.assertEqual((False, []), s.is_two_pair(gen))
 
     def test_is_two_pair3(self):
         # Test if method detects 2 pair in four of a kind
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(13, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(13, 3), Card(2, 2), Card(3, 3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_two_pair(gen))
+        self.assertEqual((True, [14, 14, 14, 14, 13]), s.is_two_pair(gen))
 
     def test_is_two_pair4(self):
         # Test if method detects 2 pair in full house
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(13, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(13, 3), Card(2,2), Card(3,3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_two_pair(gen))
+        self.assertEqual((True, [14, 14, 13, 13, 14]), s.is_two_pair(gen))
+
+    def test_is_two_pair5(self):
+        # Test if method detects 2 pair
+        gen = [Card(14, 1), Card(4, 2), Card(8, 4), Card(3, 2), Card(3, 3), Card(6, 3), Card(12, 2)]
+        s = Showdown(None, None)
+        self.assertEqual((False, []), s.is_two_pair(gen))
+
+    def test_is_two_pair6(self):
+        # Test if method detects 2 pair with 3 of a kind
+        gen = [Card(14, 3), Card(14, 2), Card(2, 1), Card(8, 4), Card(3, 3), Card(2,2), Card(8, 1)]
+        s = Showdown(None, None)
+        self.assertEqual((True, [14, 14, 8, 8, 3]), s.is_two_pair(gen))
 
     def test_is_three_kind(self):
         # Test if method detects three of a kind
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3), Card(2, 2), Card(3,3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_three_kind(gen))
+        self.assertEqual((True, [14, 14, 14, 13, 12]), s.is_three_kind(gen))
 
     def test_is_three_kind2(self):
         # Test if method detects no three of a kind
-        gen = [Card(1, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3)]
+        gen = [Card(2, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3), Card(13, 2), Card(2, 1)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_three_kind(gen))
+        self.assertEqual((False, []), s.is_three_kind(gen))
 
     def test_is_three_kind3(self):
         # Test if method detects three of a kind from four kind
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(12, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(12, 3), Card(2,2), Card(3,3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_three_kind(gen))
+        self.assertEqual((True, [14, 14, 14, 14, 12]), s.is_three_kind(gen))
 
     def test_is_straight1(self):
         # Test if method detects straight
-        gen = [Card(5, 3), Card(7, 2), Card(6, 1), Card(8, 4), Card(4, 3)]
+        gen = [Card(5, 3), Card(7, 2), Card(6, 1), Card(8, 4), Card(4, 3), Card(9, 1), Card(11, 3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_straight(gen))
+        self.assertEqual((True, [9, 8, 7, 6, 5]), s.is_straight(gen))
 
     def test_is_straight2(self):
         # Test if method detects no straight
-        gen = [Card(7, 3), Card(7, 2), Card(6, 1), Card(5, 4), Card(4, 3)]
+        gen = [Card(7, 3), Card(7, 2), Card(6, 1), Card(5, 4), Card(4, 3), Card(14, 2), Card(13, 2)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_straight(gen))
+        self.assertEqual((False, []), s.is_straight(gen))
 
     def test_is_straight3(self):
         # Test if method detects edge case of wheel straight
-        gen = [Card(5, 3), Card(4, 2), Card(3, 1), Card(2, 4), Card(14, 3)]
+        gen = [Card(5, 3), Card(4, 2), Card(3, 1), Card(2, 4), Card(14, 3), Card(7, 3), Card(9, 1)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_straight(gen))
+        self.assertEqual((True, [5, 4, 3, 2, 14]), s.is_straight(gen))
 
     def test_is_flush(self):
-        gen = [Card(5, 3), Card(4, 3), Card(3, 3), Card(14, 3), Card(11, 3)]
+        gen = [Card(5, 3), Card(4, 3), Card(3, 3), Card(14, 3), Card(11, 3), Card(2, 2), Card(13, 2)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_flush(gen))
+        self.assertEqual((True, [14, 11, 5, 4, 3]), s.is_flush(gen))
 
     def test_is_flush2(self):
-        gen = [Card(5, 2), Card(4, 3), Card(3, 3), Card(14, 3), Card(11, 3)]
+        gen = [Card(5, 2), Card(4, 3), Card(3, 3), Card(14, 3), Card(11, 3), Card(2, 2), Card(6, 4)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_flush(gen))
+        self.assertEqual((False, []), s.is_flush(gen))
 
     def test_is_full_house(self):
-        gen = [Card(5, 2), Card(5, 3), Card(14, 3), Card(14, 2), Card(14, 4)]
+        gen = [Card(5, 2), Card(5, 3), Card(14, 3), Card(14, 2), Card(5, 4), Card(2, 2), Card(3, 3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_full_house(gen))
+        self.assertEqual((True, [5, 5, 5, 14, 14]), s.is_full_house(gen))
 
     def test_is_full_house2(self):
-        gen = [Card(5, 2), Card(5, 3), Card(14, 3), Card(11, 2), Card(14, 4)]
+        gen = [Card(5, 2), Card(5, 3), Card(14, 3), Card(11, 2), Card(14, 4), Card(2, 1), Card(2, 2)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_full_house(gen))
+        self.assertEqual((False, []), s.is_full_house(gen))
 
     def test_is_full_house3(self):
         # Check if Full house not found in four of a kind
-        gen = [Card(5, 2), Card(5, 3), Card(5, 1), Card(5, 4), Card(14, 4)]
+        gen = [Card(5, 2), Card(5, 3), Card(5, 1), Card(5, 4), Card(14, 4), Card(14, 1), Card(14, 2)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_full_house(gen))
+        self.assertEqual((True, [14, 14, 14, 5, 5]), s.is_full_house(gen))
 
     def test_is_four_kind(self):
         # Test if method detects three of a kind
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(12, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(12, 3), Card(2, 2), Card(3, 3)]
         s = Showdown(None, None)
-        self.assertEqual(True, s.is_four_kind(gen))
+        self.assertEqual((True, [14, 14, 14, 14, 12]), s.is_four_kind(gen))
 
     def test_is_four_kind2(self):
         # Test if method detects no three of a kind
-        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3)]
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(13, 4), Card(12, 3), Card(2, 1), Card(2, 2)]
         s = Showdown(None, None)
-        self.assertEqual(False, s.is_four_kind(gen))
+        self.assertEqual((False, []), s.is_four_kind(gen))
 
-    def test_find_possible_combos(self):
-        gen1 = self.generate_random_cards(11)
-        players = [Player([gen1[0], gen1[1]]), Player([gen1[2], gen1[3]]), Player([gen1[9], gen1[10]])]
-        board = [gen1[4], gen1[5], gen1[6], gen1[7], gen1[8]]
-        s = Showdown(players, board)
-        combos = s.find_possible_combos(Player([gen1[0], gen1[1]]))
-        # for x in combos:
-        #      print(s.retrieve_values(x[0]))
-        #      print(len(combos))
+    def test_is_straight_flush1(self):
+        # Test if method detects no straight flush
+        gen = [Card(14, 3), Card(14, 2), Card(14, 1), Card(14, 4), Card(12, 3), Card(2, 2), Card(3, 3)]
+        s = Showdown(None, None)
+        self.assertEqual((False, []), s.is_straight_flush(gen))
 
-    def test_find_best_high_card(self):
-        gen = [Card(2, 3), Card(3, 2), Card(14, 1), Card(13, 4), Card(12, 3), Card(5, 3), Card(9, 4)]
-        players = [Player([gen[1], gen[0]])]
-        board = gen[2:]
-        s = Showdown(players, board)
-        self.assertEqual([BoardScore.high_card, 14, 13, 12, 9, 5], s.find_best(players[0]))
+    def test_is_straight_flush2(self):
+        # Test if method detects straight flush
+        gen = [Card(14, 3), Card(11, 3), Card(12, 3), Card(13, 3), Card(10, 3), Card(2, 1), Card(2, 2)]
+        s = Showdown(None, None)
+        self.assertEqual((True, [14, 13, 12, 11, 10]), s.is_straight_flush(gen))
 
-    def test_find_best_pair(self):
-        gen = [Card(2, 3), Card(3, 2), Card(14, 1), Card(14, 4), Card(12, 3), Card(5, 3), Card(9, 4)]
-        players = [Player([gen[1], gen[0]])]
-        board = gen[2:]
-        s = Showdown(players, board)
-        self.assertEqual([BoardScore.pair, 14, 13, 12, 9, 0], s.find_best(players[0]))
+    def test_is_straight_flush3(self):
+        # Test if method detects straight flush with 6
+        gen = [Card(9, 3), Card(11, 3), Card(12, 3), Card(13, 3), Card(10, 3), Card(14, 3), Card(2, 2)]
+        s = Showdown(None, None)
+        self.assertEqual((True, [14, 13, 12, 11, 10]), s.is_straight_flush(gen))
+
+    def test_is_straight_flush4(self):
+        # Test if method detects no straight flush when straight and flush present
+        gen = [Card(9, 3), Card(11, 4), Card(12, 3), Card(13, 3), Card(10, 3), Card(14, 3), Card(2, 2)]
+        s = Showdown(None, None)
+        self.assertEqual((False, []), s.is_straight_flush(gen))
+
+    # def test_find_possible_combos(self):
+    #     gen1 = self.generate_random_cards(11)
+    #     players = [Player([gen1[0], gen1[1]]), Player([gen1[2], gen1[3]]), Player([gen1[9], gen1[10]])]
+    #     board = [gen1[4], gen1[5], gen1[6], gen1[7], gen1[8]]
+    #     s = Showdown(players, board)
+    #     combos = s.find_possible_combos(Player([gen1[0], gen1[1]]))
+    #     # for x in combos:
+    #     #      print(s.retrieve_values(x[0]))
+    #     #      print(len(combos))
+    #
+    # def test_find_best_high_card(self):
+    #     gen = [Card(2, 3), Card(3, 2), Card(14, 1), Card(13, 4), Card(12, 3), Card(5, 3), Card(9, 4)]
+    #     players = [Player([gen[1], gen[0]])]
+    #     board = gen[2:]
+    #     s = Showdown(players, board)
+    #     self.assertEqual([BoardScore.high_card, 14, 13, 12, 9, 5], s.find_best(players[0]))
+    #
+    # def test_find_best_pair(self):
+    #     gen = [Card(2, 3), Card(3, 2), Card(14, 1), Card(14, 4), Card(12, 3), Card(5, 3), Card(9, 4)]
+    #     players = [Player([gen[1], gen[0]])]
+    #     board = gen[2:]
+    #     s = Showdown(players, board)
+    #     self.assertEqual([BoardScore.pair, 14, 13, 12, 9, 0], s.find_best(players[0]))
 
 if __name__ == '__main__':
     unittest.main()
